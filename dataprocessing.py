294987a74da35886b4c29108a79782dc25858d21
rreed33 <rreed33@gatech.edu>
2018-09-19 01:40:53 -0400

    This commit adds the initial version of the data processing file

---
+++ dataprocessing.py
@@ -0,0 +1,67 @@
+import argparse
+import sklearn
+import pandas as pd  
+import numpy as np  
+import matplotlib.pyplot as plt
+import math
+
+def distance(lat1, lon1, lat2, lon2):
+    radius = 6371 # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
+
+def edit(dataframe):
+	#use this function to organize all the edits to the raw data before processing
+	print(dataframe.dtypes)
+	# print(dataframe['No_Show'])
+
+	#get the bird's eye distance to from home to office
+	dataframe['distance'] = np.vectorize(distance)(dataframe['Patient_Latitude'], -1*dataframe['Patient_Longitude'],
+										dataframe['Dept_Location_Latitude'], -1*dataframe['Dept_Location_Longitude'] )
+
+	#first let's see how many appointments each person has had up until then and hwo many they miseed
+	dataframe['count_app'] 		= dataframe.groupby('Sibley_ID', sort = 'Appt_Date').cumcount()
+	dataframe['count_miss']		= dataframe[dataframe['Appt_Status_ID']==1].groupby('Sibley_ID', sort = 'Appt_Date').cumcount()
+	dataframe['count_cancel']	= dataframe[(dataframe['Appt_Status_ID'] != 2 )& (dataframe['Appt_Status_ID'] != 4)].groupby('Sibley_ID', sort = 'Appt_Date').cumcount()
+	dataframe['No_Show'] 		= (dataframe['Appt_Status_ID']==4).astype(int)
+	# print(dataframe.groupby('Sibley_ID', sort='Appt_Date').cumcount())
+
+
+	#calculate bird eye distance 
+	return dataframe
+
+
+def main():
+	df = pd.read_csv("../ENCOUNTERS_RAW.csv")
+	df_dept = pd.read_csv('../DEPT_RAW.csv')
+	df = df.merge(df_dept, on = 'Dept_ID') 
+	print(df.head())
+	df = edit(df)
+
+	df[['Payor_Type_ID']] = df[['Payor_Type_ID']].fillna(value=0)
+	df = df.drop([ 'Num_Canceled_Encounters_Since',
+                       'Num_No_Show_Encounters_Since',
+                       'Num_Canceled_Encounters_AllTime',
+                       'Num_No_Show_Encounters_AllTime'],
+                         axis = 1)
+                    
+	df.to_csv('../choa_intermediate.csv')
+
+if __name__ == '__main__':
+	main()
+
+
+#TO DO:
+# confirm categorical feature behavior in log reg model
+# look into log reg theory
+# figure out significance some how?
+#  find ou twhy the acc diff after changing the data
+# fiure out the kernel for log reg
+# make more visualizations
