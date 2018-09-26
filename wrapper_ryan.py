9f4fac7015ddf236d9673947e86d978a553b300c
rreed33 <rreed33@gatech.edu>
2018-09-26 11:32:28 -0400

    This commit adds wrapper_ryan

---
+++ wrapper_ryan.py
@@ -0,0 +1,26 @@
+# this wrapper script will run all the combinations of inputs
+# for the model in NN_austin.py
+
+import os
+
+test_size	= [.2]
+one_hot		= [False]
+groups		= ['all', 'historical', 'nonhistorical']
+no_cancel	= [True, False]
+sample_type	= ['none']
+
+for size in test_size:
+	for hot in one_hot:
+		for group in groups:
+			for can in no_cancel:
+				for typ in sample_type:
+					print('RUNNING {}'.format("python NN_austin.py -test_size {} -one_hot {} -group {} \
+								-no_cancel {} -sample_type {}".format(size, hot, group, can, typ)))
+					os.system("python NN_austin.py -test_size {} -one_hot {} -group {} \
+								-no_cancel {} -sample_type {}".format(size, hot, group, can, typ))
+					os.system("git add .")
+					os.system("git commit -m \"This commit: {}\"".format(', '.join([
+											str(size), str(hot), str(group), str(can), str(typ)])))
+					os.system("git push origin master")
+					print('\n'*5)
+
