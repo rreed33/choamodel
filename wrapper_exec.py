f99e72d4c13501d6e7f6791f4ccdda1b662082e4
windsor929 <windsor929@gmail.com>
2018-09-25 22:29:59 -0400

    This commit: 0.2, True, all, True, over_under

--- wrapper_exec.py
+++ wrapper_exec.py
@@ -1,7 +1,7 @@
 # this wrapper script will run all the combinations of inputs
 # for the model in NN_austin.py
 
-import sys
+import os
 
 test_size	= [.2, .5]
 one_hot		= [True, False]
@@ -14,10 +14,10 @@ for size in test_size:
 		for group in groups:
 			for can in no_cancel:
 				for typ in sample_type:
-					os.sytem("python NN_austin.py -test_size {} -one_hot {} -group {} \
+					os.system("python NN_austin.py -test_size {} -one_hot {} -group {} \
 								-no_cancel {} -sample_type {}".format(size, hot, group, can, typ))
 					os.system("git add .")
 					os.system("git commit -m \"This commit: {}\"".format(', '.join([
-											size, hot, group, can, typ])))
+											str(size), str(hot), str(group), str(can), str(typ)])))
 					os.system("git push origin master")
 
